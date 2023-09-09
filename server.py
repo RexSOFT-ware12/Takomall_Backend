13c93d02f780b3387488152fbcd3a3e5840a6848
Rexton Itsiah <71719147+RexSOFT-ware12@users.noreply.github.com>
2023-09-09 06:49:46 -1200

    Add files via upload

--- server.py
+++ server.py
@@ -7,8 +7,6 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-# Define CORS settings for production and development environments
-# Adjust the allowed origins accordingly
 CORS(
     app,
     origins=['https://takomall-backend.onrender.com'], 
@@ -23,63 +21,82 @@ def home():
 @app.route('/signup', methods=['POST'])
 async def signup_route():
     try:
-        if request.headers['Content-Type'] != 'application/json':
+        if request.headers.get('Content-Type') != 'application/json':
             return jsonify({'error': 'Invalid Content-Type, must be application/json'}), 400
         
         data = request.get_json()
         if not data:
             return jsonify({'error': 'Invalid JSON data in the request'}), 400
+
+        # Debugging: Print the received data
+        print("Received data:", data)
         
         response, status_code = await signup(data)
         return response, status_code
     except Exception as e:
+        # Debugging: Print the exception
+        print("Error:", str(e))
         return jsonify({'error': str(e)}), 500
 
 @app.route('/login', methods=['POST'])
 async def login_route():
-    # try:
-    if request.headers['Content-Type'] != 'application/json':
-        return jsonify({'error': 'Invalid Content-Type, must be application/json'}), 400
-    
-    data = request.get_json()
-    if not data:
-        return jsonify({'error': 'Invalid JSON data in the request'}), 400
-    
-    response, status_code = await login(data)
-    return response, status_code
-    # except Exception as e:
-    #     return jsonify({'error': str(e)}), 500
-
-@app.route('/reset_password', methods=['POST'])
-async def reset_route():
     try:
-        if request.headers['Content-Type'] != 'application/json':
+        if request.headers.get('Content-Type') != 'application/json':
             return jsonify({'error': 'Invalid Content-Type, must be application/json'}), 400
         
         data = request.get_json()
         if not data:
             return jsonify({'error': 'Invalid JSON data in the request'}), 400
+
+        # Debugging: Print the received data
+        print("Received data:", data)
+
+        response, status_code = await login(data)
+        return response, status_code
+    except Exception as e:
+        # Debugging: Print the exception
+        print("Error:", str(e))
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/reset_password', methods=['POST'])
+async def reset_route():
+    try:
+        if request.headers.get('Content-Type') != 'application/json':
+            return jsonify({'error': 'Invalid Content-Type, must be application/json'}), 400
         
+        data = request.get_json()
+        if not data:
+            return jsonify({'error': 'Invalid JSON data in the request'}), 400
+
+        # Debugging: Print the received data
+        print("Received data:", data)
+
         response, status_code = await reset_password_request(data)
         return response, status_code
     except Exception as e:
+        # Debugging: Print the exception
+        print("Error:", str(e))
         return jsonify({'error': str(e)}), 500
 
 @app.route('/reset_password/<reset_token>', methods=['POST'])
 async def reset_route_link(reset_token):
     try:
-        if request.headers['Content-Type'] != 'application/json':
+        if request.headers.get('Content-Type') != 'application/json':
             return jsonify({'error': 'Invalid Content-Type, must be application/json'}), 400
         
         data = request.get_json()
         if not data:
             return jsonify({'error': 'Invalid JSON data in the request'}), 400
-        
+
+        # Debugging: Print the received data
+        print("Received data:", data)
+
         response, status_code = await reset_link(data, reset_token)
         return response, status_code
     except Exception as e:
+        # Debugging: Print the exception
+        print("Error:", str(e))
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
-
