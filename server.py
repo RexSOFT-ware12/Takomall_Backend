3ab82c978a27cd59331dcc740201b820e790d90e
Rexton Itsiah <71719147+RexSOFT-ware12@users.noreply.github.com>
2023-09-09 05:50:04 -1200

    Add files via upload

--- server.py
+++ server.py
@@ -1,40 +1,70 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from src.resolvers.sign_up.sign_up import signup
 from src.resolvers.login.login import login
 from src.resolvers.reset_password.reset_password import reset_password_request
 from src.resolvers.reset_password.reset_link import reset_link
+from flask_cors import CORS
+
 app = Flask(__name__)
 
+CORS(
+    app,
+    origins=['http://127.0.0.1:5000', 'https://takomall-backend.onrender.com'],
+    supports_credentials=True,
+    allow_headers=['Authorization']
+)
 
 @app.route('/', methods=['POST'])
 def home():
     return 'Hello'
-    
+
 @app.route('/signup', methods=['POST'])
 async def signup_route():
-    data = request.get_json()
-    response, status_code = await signup(data)
-    return response, status_code  
+    try:
+        data = request.get_json()
+        if not data:
+            return jsonify({'error': 'Invalid JSON data in the request'}), 400
+        
+        response, status_code = await signup(data)
+        return response, status_code
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/login', methods=['POST'])
 async def login_route():
-    data = request.get_json()
-    response, status_code = await login(data)
-    return response, status_code  
-
+    try:
+        data = request.get_json()
+        if not data:
+            return jsonify({'error': 'Invalid JSON data in the request'}), 400
+        
+        response, status_code = await login(data)
+        return response, status_code
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/reset_password', methods=['POST'])
 async def reset_route():
-    data = request.get_json()
-    response, status_code = await reset_password_request(data)
-    return response, status_code 
-
+    try:
+        data = request.get_json()
+        if not data:
+            return jsonify({'error': 'Invalid JSON data in the request'}), 400
+        
+        response, status_code = await reset_password_request(data)
+        return response, status_code
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 @app.route('/reset_password/<reset_token>', methods=['POST'])
 async def reset_route_link(reset_token):
-    data = request.get_json()
-    response, status_code = await reset_link(data, reset_token)
-    return response, status_code 
+    try:
+        data = request.get_json()
+        if not data:
+            return jsonify({'error': 'Invalid JSON data in the request'}), 400
+        
+        response, status_code = await reset_link(data, reset_token)
+        return response, status_code
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False)  
+    app.run(debug=True)
