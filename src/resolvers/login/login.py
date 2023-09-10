ce22e95c795b101cff763e3cf6a36112859773bb
Rexton Itsiah <71719147+RexSOFT-ware12@users.noreply.github.com>
2023-09-09 18:37:04 -1200

    Add files via upload

--- src/resolvers/login/login.py
+++ src/resolvers/login/login.py
@@ -1,13 +1,11 @@
 
 import bcrypt
 from prisma import Prisma
-from flask import request, jsonify
+from flask import jsonify
 from Helpers.prisma_connection  import connect_to_prisma
 from Helpers.verify_password import verify_password
 
 prisma = Prisma()
-
-
 
 
 async def login(data):
@@ -16,7 +14,7 @@ async def login(data):
         password = data.get('password')
 
         user = await prisma.user.find_first(where={'email': email})
-
+        print("User:", user)
         if user and await verify_password(password, user.password):
             return jsonify({'message': 'Login successful', 'user_id': user.id}), 200
         else:
