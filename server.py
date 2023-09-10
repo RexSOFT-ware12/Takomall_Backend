5563919b97588e2a0b4393a1a3c9beb708de7f73
Rexton Itsiah <71719147+RexSOFT-ware12@users.noreply.github.com>
2023-09-09 19:10:24 -1200

    Add files via upload

--- server.py
+++ server.py
@@ -44,30 +44,29 @@ async def signup_route():
 
 @app.route('/login', methods=['POST'])
 async def login_route():
-    try:
-        if request.headers.get('Content-Type') != 'application/json':
-            return jsonify({'error': 'Invalid Content-Type, must be application/json'}), 400
-        
-        data = request.get_json()
-        if not data:
-            return jsonify({'error': 'Invalid JSON data in the request'}), 400
+    if request.headers.get('Content-Type') != 'application/json':
+        return jsonify({'error': 'Invalid Content-Type, must be application/json'}), 400
+    
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'Invalid JSON data in the request'}), 400
 
-        # Debugging: Print the received data
-        print("Received data:", data)
-        if await connect_to_prisma(prisma):
-            email = data.get('email')
-            password = data.get('password')
+    # Debugging: Print the received data
+    print("Received data:", data)
+    if await connect_to_prisma(prisma):
+        email = data.get('email')
+        password = data.get('password')
 
-            user = await prisma.user.find_first(where={'email': email})
-            print("User:", user)
-            if user and await verify_password(password, user.password):
-                return jsonify({'message': 'Login successful', 'user_id': user.id}), 200
-            else:
-                return jsonify({'message': 'Invalid credentials'}), 401
-    except Exception as e:
-        # Handle exceptions that occur outside of the try block
-        print("Error:", str(e))
-        return jsonify({'error': str(e)}), 500
+        user = await prisma.user.find_first(where={'email': email})
+        print("User:", user)
+        if user and await verify_password(password, user.password):
+            return jsonify({'message': 'Login successful', 'user_id': user.id}), 200
+        else:
+            return jsonify({'message': 'Invalid credentials'}), 401
+
+    # Return a generic error response if an unexpected error occurs
+    return jsonify({'error': 'An unexpected error occurred'}), 500
+
 
 
 @app.route('/reset_password', methods=['POST'])
